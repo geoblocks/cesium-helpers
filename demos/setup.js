d802efcb08c4a6236cddaa3eed9c40f3b2499675
Frederic Junod <frederic.junod@camptocamp.com>
2023-07-21 09:23:58 +0200

    Use swisstopo imagery for the demos

--- demos/setup.js
+++ demos/setup.js
@@ -1,7 +1,12 @@
-// window['CESIUM_BASE_URL'] = 'node_modules/cesium/Build/Cesium/';
-// import {Viewer, ImageryLayer, OpenStreetMapImageryProvider, CesiumTerrainProvider, Cartesian3, Math as CesiumMath} from 'cesium';
+const SWITZERLAND_RECTANGLE = Cesium.Rectangle.fromDegrees(4, 45, 12, 48);
 
 export async function createViewer(container) {
+  Object.assign(Cesium.RequestScheduler.requestsByServer, {
+    "wmts.geo.admin.ch:443": 18,
+    "download.swissgeol.ch:443": 18,
+    "tile.openstreetmap.org:443": 18,
+  });
+
   const viewer = new Cesium.Viewer(container, {
     timeline: false,
     navigationHelpButton: false,
@@ -12,21 +17,27 @@ export async function createViewer(container) {
     homeButton: false,
     fullscreenButton: false,
     baseLayer: new Cesium.ImageryLayer(
-      new Cesium.OpenStreetMapImageryProvider({
-        url: "https://tile.openstreetmap.org/",
+      new Cesium.UrlTemplateImageryProvider({
+        url: "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/3857/{z}/{x}/{y}.jpeg",
+        rectangle: SWITZERLAND_RECTANGLE,
       })
     ),
     terrainProvider: await Cesium.CesiumTerrainProvider.fromUrl(
       "https://download.swissgeol.ch/cli_terrain/ch-2m/"
     ),
   });
+
+  viewer.scene.highDynamicRange = true;
+  viewer.scene.globe.showGroundAtmosphere = true;
+  viewer.scene.globe.enableLighting = true;
+
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(6.06749, 43.77784, 204227),
     orientation: {
       heading: Cesium.Math.toRadians(26.0),
-      pitch: Cesium.Math.toRadians(-33.0)
+      pitch: Cesium.Math.toRadians(-33.0),
     },
-    duration: 0
+    duration: 0,
   });
   return viewer;
 }
