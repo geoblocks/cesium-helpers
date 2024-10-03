d611a9e8f550c35c30346176aab3c6e4c289c633
Frederic Junod <frederic.junod@camptocamp.com>
2024-10-03 14:43:59 +0200

    Updates

---
+++ cesium-compass-bar.1db6e123.js
@@ -0,0 +1,89 @@
+var e=globalThis,t={},i={},s=e.parcelRequire5532;null==s&&((s=function(e){if(e in t)return t[e].exports;if(e in i){var s=i[e];delete i[e];var r={id:e,exports:{}};return t[e]=r,s.call(r.exports,r,r.exports),r.exports}var a=Error("Cannot find module '"+e+"'");throw a.code="MODULE_NOT_FOUND",a}).register=function(e,t){i[e]=t},e.parcelRequire5532=s),s.register;var r=s("800sp");class a extends r.LitElement{static get properties(){return{scene:{type:Object},heading:{type:Number}}}static get styles(){return(0,r.css)`
+      :host {
+        --cesium-compass-bar-tick-color: #000;
+        --cesium-compass-bar-intercardinal-width: 100px;
+      }
+      :host * {
+        box-sizing: content-box;
+      }
+      .container {
+        display: flex;
+        justify-content: center;
+        height: 100%;
+        overflow: hidden;
+      }
+      .compass-bar {
+        display: flex;
+      }
+      .compass-bar > div {
+        width: var(--cesium-compass-bar-intercardinal-width);
+        text-align: center;
+      }
+      .label {
+        padding: 4px;
+        color: var(--cesium-compass-bar-tick-color);
+      }
+      .ticks {
+        display: flex;
+        justify-content: space-around;
+        align-items: flex-end;
+      }
+      .ticks > div {
+        width: 1px;
+        height: 6px;
+        background-color: var(--cesium-compass-bar-tick-color);
+      }
+      .ticks > div:nth-child(4) {
+        height: 10px;
+      }
+      .center-tick {
+        margin: auto;
+        width: 4px;
+        height: 4px;
+        border-radius: 50%;
+        background-color: var(--cesium-compass-bar-tick-color);
+      }
+    `}constructor(){super(),this.scene,this.intercardinalWidth,this.unlistenFromPostRender=null}updated(){this.scene&&!this.unlistenFromPostRender&&(this.unlistenFromPostRender=this.scene.postRender.addEventListener(()=>{this.heading=this.scene.camera.heading}),this.intercardinalWidth=parseInt(getComputedStyle(this).getPropertyValue("--cesium-compass-bar-intercardinal-width")))}getTransform(e){let t=this.intercardinalWidth,i=-t/2,s=-this.heading/(Math.PI/4)+4,r=s-e;return r<-4&&(i+=8*t),r>4&&(i-=8*t),`transform: translate(${s*t+i}px)`}render(){let e=(0,r.html)`
+      <div class="ticks">
+        ${Array(7).fill().map(()=>(0,r.html)`<div part="tick"></div>`)}
+      </div>
+    `;return(0,r.html)`
+      <div class="container">
+        <div class="compass-bar">
+          <div style=${this.getTransform(4)}>
+            <div class="label" part="label">N</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(3)}>
+            <div class="label" part="label">NE</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(2)}>
+            <div class="label" part="label">E</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(1)}>
+            <div class="label" part="label">SE</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(0)}>
+            <div class="label" part="label">S</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(-1)}>
+            <div class="label" part="label">SW</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(-2)}>
+            <div class="label" part="label">W</div>
+            ${e}
+          </div>
+          <div style=${this.getTransform(-3)}>
+            <div class="label" part="label">NW</div>
+            ${e}
+          </div>
+        </div>
+      </div>
+      <div class="center-tick" part="center-tick"></div>
+    `}}customElements.define("cesium-compass-bar",a);
+//# sourceMappingURL=cesium-compass-bar.1db6e123.js.map
