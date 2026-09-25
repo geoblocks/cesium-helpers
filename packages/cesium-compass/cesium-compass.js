069368293e2f185f4e70d8a5f8a9f17c4adf6b76
Frederic Junod <frederic.junod@camptocamp.com>
2026-09-25 09:14:29 +0200

    chore: enable stricter type checking options
    Enable noImplicitReturns, noUnusedLocals, noUnusedParameters and
    exactOptionalPropertyTypes while the code is still JavaScript, so the
    TypeScript migration does not surface them all at once. Drop the unused
    event parameters they reported in cesium-compass.

--- packages/cesium-compass/cesium-compass.js
+++ packages/cesium-compass/cesium-compass.js
@@ -287,10 +287,7 @@ class CesiumCompass extends LitElement {
     this.rotateClick = false;
   }
 
-  /**
-   * @param {PointerEvent} event
-   */
-  handleRotatePointerUp(event) {
+  handleRotatePointerUp() {
     document.removeEventListener('pointermove', this.handleRotatePointerMoveFunction, false);
     document.removeEventListener('pointerup', this.handleRotatePointerUpFunction, false);
 
@@ -415,10 +412,7 @@ class CesiumCompass extends LitElement {
 
   }
 
-  /**
-   * @param {PointerEvent} event
-   */
-  handleOrbitPointerUp(event) {
+  handleOrbitPointerUp() {
     document.removeEventListener('pointermove', this.handleOrbitPointerMoveFunction, false);
     document.removeEventListener('pointerup', this.handleOrbitPointerUpFunction, false);
     if (this.unlistenFromClockTick) {
